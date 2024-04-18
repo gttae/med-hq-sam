edfd6257e31d9607736dd177b6ad7635ef02e727
gttae <gtgtgt411@gmail.com>
2024-04-19 00:57:04 +0900

    5
    5

--- modeling/mask_decoder_hq.py
+++ modeling/mask_decoder_hq.py
@@ -168,11 +168,12 @@ class MaskDecoderHQ(nn.Module):
         output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight, self.hf_token.weight], dim=0)
         output_tokens = output_tokens.unsqueeze(0).expand(sparse_prompt_embeddings.size(0), -1, -1)
         tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
+        
 
         # Expand per-image data in batch direction to be per-mask
-        src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
+        src = torch.repeat_interleave(image_embeddings, tokens.shape[0] // image_embeddings.shape[0], dim=0)
         src = src + dense_prompt_embeddings
-        pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
+        pos_src = torch.repeat_interleave(image_pe, tokens.shape[0] // image_pe.shape[0], dim=0)
         b, c, h, w = src.shape
 
         # Run the transformer
